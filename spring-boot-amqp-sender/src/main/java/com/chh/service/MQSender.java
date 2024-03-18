ea0a8b7c3432b71a5fff1d21101b0802f63b24bc
陈汉辉 <chenhanhui@szydtx.com>
2024-03-18 16:09:03 +0800

    【优化】日志打印

--- spring-boot-amqp-sender/src/main/java/com/chh/service/MQSender.java
+++ spring-boot-amqp-sender/src/main/java/com/chh/service/MQSender.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service;
 /**
  * @author 陈汉辉
  * @since 2024-03-15 星期五 14:12:51
- * <p>
+ *
  * 消息发送者
  */
-//@Slf4j
+@Slf4j
 @Service
 public class MQSender {
 
@@ -22,9 +22,9 @@ public class MQSender {
      * 发送消息
      */
     public void send(Object msg) {
-//        log.info("发送消息-" + msg);
-        //没有指定交换机会走默认的交换机，AMQP default
-        //AMQP default是一个direct路由模式的交换机
+        log.info("发送消息-" + msg);
+        // 没有指定交换机会走默认的交换机，AMQP default
+        // AMQP default是一个direct路由模式的交换机
         rabbitTemplate.convertAndSend("queue", msg);
     }
 
